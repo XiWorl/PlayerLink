e24be59aded9216f522c913da422eb66901c66cc
XiWorl <97926984+XiWorl@users.noreply.github.com>
2025-06-27 15:54:13 -0700

    Add page routing
    This commit adds page routing from the login screen to the profile screen.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App  from './App.jsx'
-import { GoogleOAuthProvider } from '@react-oauth/google'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import App from "./App.jsx"
+import { GoogleOAuthProvider } from "@react-oauth/google"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import ProfilePage from "./components/Profile/ProfilePage"
 
 const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <GoogleOAuthProvider clientId={CLIENT_ID}>
-    <App />
-    </GoogleOAuthProvider>
-  </StrictMode>,
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<GoogleOAuthProvider clientId={CLIENT_ID}>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<App />} />
+					<Route path="/profile" element={<ProfilePage />} />
+				</Routes>
+			</BrowserRouter>
+		</GoogleOAuthProvider>
+	</StrictMode>
 )
