8a5ff6df5d89b8a3f51ccf31d27a1776f7f00a79
XiWorl <97926984+XiWorl@users.noreply.github.com>
2025-07-06 22:20:14 -0700

    Improve CSS for apply page
    This commit drastically improves the CSS for the apply page. Two application posts are currently hard coded for visual and testing purposes.

--- frontend/src/pages/ApplyPage.jsx
+++ frontend/src/pages/ApplyPage.jsx
@@ -1,26 +1,52 @@
-
+import Navbar from "../components/Navbar/Navbar"
+import "../components/TeamApplication/ApplyPage.css"
 
 export default function ApplyPage() {
-    return (
-        <>
-            <div className="header">
-                <div className="view-players">
-                    <button className="view-players-btn">View Players</button>
-                </div>
-                <div className="view-teams">
-                    <button className="view-teams-btn">View Teams</button>
-                </div>
-            </div>
-            <div className="page-content">
-                <div className="sidebar">
-                    <div className="sidebar-options">
-                        <button className="sidebar-options-btn">View Applications</button>
-                        <button className="sidebar-options-btn">Applications</button>
-                        <button className="sidebar-options-btn">Create application</button>
-                    </div>
-                </div>
-                <div className="postings"></div>
-            </div>
-        </>
-    )
+    //TODO: Two posts are currently hardcoded, need to replace with data from backend in a future commit
+	return (
+		<>
+			<Navbar />
+			<div className="apply-page">
+				<div className="header">
+					<div className="view-players">
+						<button className="view-players-btn">View Players</button>
+					</div>
+					<div className="view-teams">
+						<button className="view-teams-btn">View Teams</button>
+					</div>
+				</div>
+				<div className="page-content">
+					<div className="sidebar">
+						<div className="sidebar-options">
+							<button className="sidebar-options-btn">
+								View Applications
+							</button>
+						</div>
+					</div>
+					<div className="postings">
+						<div className="post">
+							<div className="apply-profile-picture"></div>
+							<div className="apply-details">
+								<h2>Job title</h2>
+								<div className="post-information">
+									<h3>Team name * location</h3>
+									<button>Apply</button>
+								</div>
+							</div>
+						</div>
+						<div className="post">
+							<div className="apply-profile-picture"></div>
+							<div className="apply-details">
+								<h2>Job title</h2>
+								<div className="post-information">
+									<h3>Team name * location</h3>
+									<button>Apply</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
