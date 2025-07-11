4e6c96341ad71bb00be65699d4b49ed1762e22fb
XiWorl <97926984+XiWorl@users.noreply.github.com>
2025-07-11 10:54:20 -0700

    Fix profile not loading bug
    This commit fixes a bug which occurred when navigating from one profile to another. This bug occurred because the page the user wanted to navigate to wouldn't re-render. This bug is fixed by re-rendering the page after switching to a new profile.

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -7,6 +7,9 @@ import Navbar from "../components/Navbar/Navbar"
 import UserProfile from "../components/Profile/UserProfile"
 import "../components/Profile/ProfilePage.css"
 
+const INITIAL_DISPLAY_ID = -1
+let currentAccountIdDisplayed = INITIAL_DISPLAY_ID
+
 async function displayProfileData(accountType, id, setAccountData, setIsLoading) {
 	const data = await getProfileData(accountType, id)
 	setAccountData(data)
@@ -18,9 +21,15 @@ export default function ProfilePage({ accountType }) {
 	const [isLoading, setIsLoading] = useState(true)
 	const { id } = useParams()
 
+	if (id != currentAccountIdDisplayed) {
+		setIsLoading(true)
+		displayProfileData(accountType, id, setAccountData, setIsLoading)
+	}
+
 	useEffect(() => {
 		displayProfileData(accountType, id, setAccountData, setIsLoading)
 	}, [])
+	currentAccountIdDisplayed = id
 
 	return accountType == AccountType.PLAYER ? (
 		<>
