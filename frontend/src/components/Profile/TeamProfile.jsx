ebbea1121b4a2241888c9baaee7dd396e0505787
XiWorl <97926984+XiWorl@users.noreply.github.com>
2025-07-15 10:24:25 -0700

    Remove unnecessary useContext
    This commit removes useContexts that aren't used and useState that isn't used.

--- frontend/src/components/Profile/TeamProfile.jsx
+++ frontend/src/components/Profile/TeamProfile.jsx
@@ -1,13 +1,12 @@
 import { EditProfileTextButton, TypeOfEditButton } from "./EditButton"
 import { modalSubmitHelper } from "./EditButtonUtils"
-import { createContext, useState } from "react"
+import { useState } from "react"
 import { useParams } from "react-router-dom"
 import { getAccountDataFromSessionStorage, AccountType } from "../../utils/globalUtils"
 import ApplyButton from "./ApplyButton"
 import "./ProfilePage.css"
 
 const defaultProfileInfo = ""
-export const TeamProfileContext = createContext()
 
 export default function TeamProfile({ isLoading, accountData, setIsLoading }) {
 	if (isLoading || accountData.playerId != null) {
@@ -23,75 +22,71 @@ export default function TeamProfile({ isLoading, accountData, setIsLoading }) {
 	const sessionStorageAccountData = getAccountDataFromSessionStorage()
 
 	return (
-		<TeamProfileContext.Provider value={{ setDescription, setOverview, id }}>
-			<div className="profile-page">
-				<div className="profile-banner">
-					<div className="profile-picture">
-						<div className="profile-picture-placeholder">
-							{accountData.name.charAt(0)}
-						</div>
+		<div className="profile-page">
+			<div className="profile-banner">
+				<div className="profile-picture">
+					<div className="profile-picture-placeholder">
+						{accountData.name.charAt(0)}
 					</div>
 				</div>
-				<div className="profile-header">
-					<div className="profile-info">
-						<h1 className="profile-name">{`${accountData.name}`}</h1>
-						<div className="profile-title">
-							<p className="profile-title-text">{`${
-								description || defaultProfileInfo
-							}`}</p>
-							<EditProfileTextButton
-								modalTitle={"Edit Description"}
-								onSubmitButtonClicked={(textValue) =>
-									modalSubmitHelper(
-										textValue,
-										TypeOfEditButton.DESCRIPTION,
-										AccountType.TEAM,
-										id,
-										setDescription
-									)
-								}
-								profileId={id}
-							/>
-						</div>
-						<p className="profile-location">📍 {accountData.location}</p>
-					</div>
-					<div>
-						<div>
-							<button>Home</button>
-							<button>Roster</button>
-							{sessionStorageAccountData &&
-								sessionStorageAccountData.accountType !=
-									AccountType.TEAM &&
-								sessionStorageAccountData.id !==
-									accountData.accountId && (
-									<ApplyButton
-										playerAccountId={sessionStorageAccountData.id}
-										teamAccountId={accountData.accountId}
-									/>
-								)}
-						</div>
-					</div>
-				</div>
-				<div className="profile-about">
-					<div className="profile-about-header">
-						<h3>Overview</h3>
+			</div>
+			<div className="profile-header">
+				<div className="profile-info">
+					<h1 className="profile-name">{`${accountData.name}`}</h1>
+					<div className="profile-title">
+						<p className="profile-title-text">{`${
+							description || defaultProfileInfo
+						}`}</p>
 						<EditProfileTextButton
-							modalTitle={"Edit Overview"}
+							modalTitle={"Edit Description"}
 							onSubmitButtonClicked={(textValue) =>
 								modalSubmitHelper(
-										textValue,
-										TypeOfEditButton.OVERVIEW,
-										AccountType.TEAM,
-										id,
-										setOverview
-									)
+									textValue,
+									TypeOfEditButton.DESCRIPTION,
+									AccountType.TEAM,
+									id,
+									setDescription
+								)
 							}
 							profileId={id}
 						/>
 					</div>
-					<p className="profile-about-text">{`${overview}`}</p>
+					<p className="profile-location">📍 {accountData.location}</p>
+				</div>
+				<div>
+					<div>
+						<button>Home</button>
+						<button>Roster</button>
+						{sessionStorageAccountData &&
+							sessionStorageAccountData.accountType != AccountType.TEAM &&
+							sessionStorageAccountData.id !== accountData.accountId && (
+								<ApplyButton
+									playerAccountId={sessionStorageAccountData.id}
+									teamAccountId={accountData.accountId}
+								/>
+							)}
+					</div>
 				</div>
 			</div>
-		</TeamProfileContext.Provider>
+			<div className="profile-about">
+				<div className="profile-about-header">
+					<h3>Overview</h3>
+					<EditProfileTextButton
+						modalTitle={"Edit Overview"}
+						onSubmitButtonClicked={(textValue) =>
+							modalSubmitHelper(
+								textValue,
+								TypeOfEditButton.OVERVIEW,
+								AccountType.TEAM,
+								id,
+								setOverview
+							)
+						}
+						profileId={id}
+					/>
+				</div>
+				<p className="profile-about-text">{`${overview}`}</p>
+			</div>
+		</div>
 	)
 }
--- frontend/src/components/Profile/UserProfile.jsx
+++ frontend/src/components/Profile/UserProfile.jsx
@@ -2,11 +2,9 @@ import { EditProfileTextButton, TypeOfEditButton } from "./EditButton"
 import { AccountType } from "../../utils/globalUtils"
 import { modalSubmitHelper } from "./EditButtonUtils"
 import { useParams } from "react-router-dom"
-import { createContext, useState } from "react"
+import { useState } from "react"
 import "./ProfilePage.css"
 const defaultProfileInfo = ""
-
-export const UserProfileContext = createContext()
 
 export default function UserProfile({ isLoading, accountData, setIsLoading }) {
 	if (isLoading || accountData.teamId != null) {
@@ -14,73 +12,60 @@ export default function UserProfile({ isLoading, accountData, setIsLoading }) {
 		return <h1>Loading...</h1>
 	}
 
-	useEffect(() => {
-		setLoggedInAccountData(getAccountDataFromSessionStorage())
-	}, [])
-
-	useEffect(() => {
-		setLoggedInAccountData(getAccountDataFromSessionStorage())
-	}, [])
-
 	const [bio, setBio] = useState(accountData.bio || defaultProfileInfo)
 	const [about, setAbout] = useState(accountData.about || defaultProfileInfo)
-	const [loggedInAccountData, setLoggedInAccountData] = useState(null)
 	const { id } = useParams()
 
 	return (
-		<UserProfileContext.Provider
-			value={{ setAbout, setBio, loggedInAccountData, id }}
-		>
-			<div className="profile-page">
-				<div className="profile-banner">
-					<div className="profile-picture">
-						<div className="profile-picture-placeholder">
-							{accountData.firstName.charAt(0)}
-						</div>
+		<div className="profile-page">
+			<div className="profile-banner">
+				<div className="profile-picture">
+					<div className="profile-picture-placeholder">
+						{accountData.firstName.charAt(0)}
 					</div>
 				</div>
-				<div className="profile-header">
-					<div className="profile-info">
-						<h1 className="profile-name">{`${accountData.firstName} ${accountData.lastName}`}</h1>
-						<div className="profile-title">
-							<p className="profile-title-text">{`${bio}`}</p>
-							<EditProfileTextButton
-								modalTitle={"Edit Bio"}
-								onSubmitButtonClicked={(textValue) =>
-									modalSubmitHelper(
-										textValue,
-										TypeOfEditButton.BIO,
-										AccountType.PLAYER,
-										id,
-										setBio
-									)
-								}
-								profileId={id}
-							/>
-						</div>
-						<p className="profile-location">📍 {accountData.location}</p>
-					</div>
-				</div>
-				<div className="profile-about">
-					<div className="profile-about-header">
-						<h3>About</h3>
+			</div>
+			<div className="profile-header">
+				<div className="profile-info">
+					<h1 className="profile-name">{`${accountData.firstName} ${accountData.lastName}`}</h1>
+					<div className="profile-title">
+						<p className="profile-title-text">{`${bio}`}</p>
 						<EditProfileTextButton
-							modalTitle={"Edit About"}
+							modalTitle={"Edit Bio"}
 							onSubmitButtonClicked={(textValue) =>
 								modalSubmitHelper(
 									textValue,
-									TypeOfEditButton.ABOUT,
+									TypeOfEditButton.BIO,
 									AccountType.PLAYER,
 									id,
-									setAbout
+									setBio
 								)
 							}
 							profileId={id}
 						/>
 					</div>
-					<p className="profile-about-text">{`${about}`}</p>
+					<p className="profile-location">📍 {accountData.location}</p>
 				</div>
 			</div>
-		</UserProfileContext.Provider>
+			<div className="profile-about">
+				<div className="profile-about-header">
+					<h3>About</h3>
+					<EditProfileTextButton
+						modalTitle={"Edit About"}
+						onSubmitButtonClicked={(textValue) =>
+							modalSubmitHelper(
+								textValue,
+								TypeOfEditButton.ABOUT,
+								AccountType.PLAYER,
+								id,
+								setAbout
+							)
+						}
+						profileId={id}
+					/>
+				</div>
+				<p className="profile-about-text">{`${about}`}</p>
+			</div>
+		</div>
 	)
 }
