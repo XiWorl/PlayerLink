0be640380a80e40a1ea67be9d604cf7de8cdbb1c
XiWorl <97926984+XiWorl@users.noreply.github.com>
2025-07-25 13:02:49 -0700

    Add frontend suggestions

--- frontend/src/components/TeamApplications/Utils.jsx
+++ frontend/src/components/TeamApplications/Utils.jsx
@@ -1,6 +1,37 @@
 import { getProfileData } from "../../api"
 import { AccountType } from "../../utils/globalUtils"
 
+export const SidebarTabs = {
+	APPLICATIONS: "Applications",
+	RECOMMENDATIONS: "Recommendations",
+}
+
+export function Sidebar({ accountType, setCurrentTab }) {
+	const viewApplicationsButtonText =
+		accountType === AccountType.PLAYER ? "My Applications" : "View Applications"
+
+	return (
+		<div className="sidebar">
+			<div className="sidebar-options">
+				<button
+					className="sidebar-options-btn"
+					onClick={() => setCurrentTab(SidebarTabs.APPLICATIONS)}
+				>
+					{viewApplicationsButtonText}
+				</button>
+				{accountType === AccountType.PLAYER && (
+					<button
+						className="sidebar-options-btn"
+						onClick={() => setCurrentTab(SidebarTabs.RECOMMENDATIONS)}
+					>
+						Suggested For You
+					</button>
+				)}
+			</div>
+		</div>
+	)
+}
+
 export async function createApplicationTile(applicationData) {
 	const profileInformation = await getProfileData(AccountType.TEAM, applicationData.teamAccountId)
 
