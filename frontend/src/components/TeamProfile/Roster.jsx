11459cbb6a45a70fdcf72aa3456de49385aa9d5d
XiWorl <97926984+XiWorl@users.noreply.github.com>
2025-07-25 10:16:38 -0700

    Add roster display to teams

---
+++ frontend/src/components/TeamProfile/Roster.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { AccountType } from "../../utils/globalUtils"
+import { getProfileData } from "../../api"
+
+async function addProfileToRoster(id, setRoster) {
+	const profileInformation = await getProfileData(AccountType.PLAYER, id)
+	setRoster((prevRoster) => {
+		return [...prevRoster, profileInformation]
+	})
+}
+
+export function Roster({ accountRosterIds }) {
+	const navigate = useNavigate()
+	const [roster, setRoster] = useState([])
+
+	useEffect(() => {
+		for (const id of accountRosterIds) {
+			addProfileToRoster(id, setRoster)
+		}
+	}, [])
+
+	return (
+		<div className="profile-about">
+			<div className="profile-about-header">
+				<h3>Roster</h3>
+			</div>
+			<div className="roster-container">
+				{roster.map((profileInformation, index) => {
+					return (
+						<div
+							className="roster-tile pointer"
+							key={index}
+							onClick={() => {
+								navigate(`/profiles/${profileInformation.accountId}`)
+							}}
+						>
+							<div className="apply-profile-picture"></div>
+							<div className="roster-tile-information">
+								<h4>
+									{profileInformation.firstName || ""}{" "}
+									{profileInformation.lastName || ""}
+								</h4>
+								<h5>{profileInformation.bio}</h5>
+							</div>
+						</div>
+					)
+				})}
+			</div>
+		</div>
+	)
+}
